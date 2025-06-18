ba00b058cbb4d8721b979af2023c05adc547a5d1
Cedric-Hp <cedricmouchon23hp@gmail.com>
2025-06-18 16:04:33 +0200

    Start Implementing Bombs

---
+++ src/creationBombes.js
@@ -0,0 +1,9 @@
+let bombe = document.createElement("div")
+bombe.style.width = 20 + "px";
+bombe.style.height = 20 + "px";
+bombe.style.backgroundColor = "black";
+bombe.id = "bombe"
+bombe.style.position = "absolute";
+bombe.style.top = square_2.style.top
+bombe.style.left = square_2.style.left
+document.getElementById('playground').appendChild(bombe);
--- src/script.js
+++ src/script.js
@@ -1,50 +1,147 @@
-let square = document.querySelector("#block")
-let movingBy = 50
+let square_1 = document.querySelector("#block_1")
+let square_2 = document.querySelector("#block_2")
+const movingBy = 2
 
+// Loader
 window.addEventListener("load", () => {
-    square.style.position = "relative";
-    square.style.left = 0;
-    square.style.top = 0;
+    square_1.style.position = "absolute";
+    square_1.style.left = 0 + "px";
+    square_1.style.top = 0 + "px";
+    square_2.style.position = "absolute";
+    square_2.style.left = 880 + "px";
+    square_2.style.top = 880 + "px";
 })
-
-const movement = (object, amount, direction) => {
-
+const objList = [square_1, square_2]
+const controller = {
+    38: {pressed: false, name: 38},
+    40: {pressed: false, name: 40},
+    37: {pressed: false, name: 37},
+    39: {pressed: false, name: 39},
+    90: {pressed: false, name: 90},
+    83: {pressed: false, name: 83},
+    81: {pressed: false, name: 81},
+    68: {pressed: false, name: 68},
+    32: {pressed: false, name: 32},
+    96: {pressed: false, name: 96}
 }
-window.addEventListener("keydown", (e) => {
-    switch (e.key) {
-        case "ArrowUp":
-            if (parseInt(square.style.top) == 0) {
-                square.style.top = 850 + "px";
+const hitBoxchecker = (direction, xObj_1, yObj_1, xObj_2, yObj_2) => {
+    switch(direction) {
+        case "UP":
+            if( ( yObj_1 - yObj_2 <= 20 && yObj_1 - yObj_2 > -20 ) && (xObj_1 - xObj_2 < 20 && xObj_1 - xObj_2 > -20) ) {
+                return false
             }
             else {
-                square.style.top = parseInt(square.style.top) - movingBy + "px";
+                return true
             }
             break;
-        case "ArrowDown":
-            if (parseInt(square.style.top) == 850) {
-                square.style.top = 0 + "px";
+        case "DOWN":
+            if( ( yObj_2 - yObj_1 <= 20 && yObj_2 - yObj_1 > -20 ) && (xObj_1 - xObj_2 < 20 && xObj_1 - xObj_2 > -20) ) {
+                return false
             }
             else {
-                square.style.top = parseInt(square.style.top) + movingBy + "px";
+                return true
             }
             break;
-        case "ArrowLeft":
-            if (parseInt(square.style.left) == 0) {
-                square.style.left = 850 + "px";
+        case "LEFT":
+            if( ( xObj_1 - xObj_2 <= 20 && xObj_1 - xObj_2 > -20 ) && (yObj_1 - yObj_2 < 20 && yObj_1 - yObj_2 > -20) ) {
+                return false
             }
             else {
-                square.style.left = parseInt(square.style.left) - movingBy + "px";
+                return true
             }
             break;
-        case "ArrowRight":
-            if (parseInt(square.style.left) == 850) {
-                square.style.left = 0 + "px";
+        case "RIGHT":
+            if( (xObj_2 - xObj_1 <= 20 && xObj_2 - xObj_1 > -20) && (yObj_1 - yObj_2 < 20 && yObj_1 - yObj_2 > -20) ) {
+                return false
             }
             else {
-                square.style.left = parseInt(square.style.left) + movingBy + "px";
+                return true
             }
             break;
     }
+}
+
+const objColliderChecker = (obj, direction) => {
+    for (let objTested of objList) {
+        if (obj != objTested) {
+            return hitBoxchecker(direction, parseInt(obj.style.left), parseInt(obj.style.top), parseInt(objTested.style.left), parseInt(objTested.style.top) )
+        }
+    }
+
+}
+
+const moveplayer = (keyInput) => {
+
+        switch (keyInput) {
+            case 38: // Flèche du haut
+                if (parseInt(square_1.style.top) != 0 && objColliderChecker(square_1, "UP")) {
+                    square_1.style.top = parseInt(square_1.style.top) - movingBy + "px";
+                }
+                break;
+            case 40: // Flèche du bas
+                if (parseInt(square_1.style.top) != 880 && objColliderChecker(square_1, "DOWN")) {
+                    square_1.style.top = parseInt(square_1.style.top) + movingBy + "px";
+                }
+                break;
+            case 37: // Flèche de Gauche
+                if (parseInt(square_1.style.left) != 0 && objColliderChecker(square_1, "LEFT")) {
+                    square_1.style.left = parseInt(square_1.style.left) - movingBy + "px";
+                }
+                break;
+            case 39: // Flèche de droite
+                if (parseInt(square_1.style.left) != 880 && objColliderChecker(square_1, "RIGHT")) {
+                    square_1.style.left = parseInt(square_1.style.left) + movingBy + "px";
+                }
+                break;
+            case 90: // Z
+                if (parseInt(square_2.style.top) != 0 && objColliderChecker(square_2, "UP")) {
+                    square_2.style.top = parseInt(square_2.style.top) - movingBy + "px";
+                }
+                break;
+            case 83: // S
+                if (parseInt(square_2.style.top) != 880 && objColliderChecker(square_2, "DOWN")) {
+                    square_2.style.top = parseInt(square_2.style.top) + movingBy + "px";
+                }
+                break;
+            case 81: // Q
+                if (parseInt(square_2.style.left) != 0 && objColliderChecker(square_2, "LEFT")) {
+                    square_2.style.left = parseInt(square_2.style.left) - movingBy + "px";
+                }
+                break;
+            case 68: // D
+                if (parseInt(square_2.style.left) != 880 && objColliderChecker(square_2, "RIGHT")) {
+                    square_2.style.left = parseInt(square_2.style.left) + movingBy + "px";
+                }
+                break;
+            case 32: // SPACE
+            
+                break;
+            case 96: // NUMPAD_0
+                
+                break;
+        }
+}
+
+document.addEventListener("keydown", (e) => {
+    if (controller[e.keyCode]) {
+        controller[e.keyCode].pressed = true
+    }
+})
+document.addEventListener("keyup", (e) => {
+    if (controller[e.keyCode]) {
+        controller[e.keyCode].pressed = false
+    }
 })
 
+const executeMoves = () => {
+    for(let Key in controller) {
+        if(controller[Key].pressed)
+            moveplayer(controller[Key].name)
+    }
+}
 
+const animate = () => {
+    executeMoves()
+    window.requestAnimationFrame(animate)
+}
+window.requestAnimationFrame(animate)
